2867e0ca1ede903a7d5ff3a0c5b894ca7a30083c
akscodebank <akshayappu7147@gmail.com>
2020-08-11 14:42:28 +0530

    commit 2

--- src/Feed.js
+++ src/Feed.js
@@ -1,15 +1,70 @@
 import React from 'react';
+import "./Feed.css";
 
 function Feed() {
     return (
-        <div>
+        <div className="feed">
             {/* Header */}
             <div classsName="feed">
             <h2>Home</h2>
             </div>
 
             {/* TweetBox */}
-
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
+            <h3>Post #</h3>
             {/* Post */}
             {/* Post */}
             {/* Post */}
