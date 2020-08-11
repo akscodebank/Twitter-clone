0ebb548d38d72d728d04fee9a48e3e881fc4a535
Akshay-Anand <akshayappu7147@gmail.com>
2020-08-11 13:51:23 +0530

    commit 1

---
+++ src/Feed.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Feed() {
+    return (
+        <div>
+            {/* Header */}
+            <div classsName="feed">
+            <h2>Home</h2>
+            </div>
+
+            {/* TweetBox */}
+
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            {/* Post */}
+            </div>
+
+    );
+}
+
+export default Feed;
