dc7bc252eed9874fe4e990f319616b4079255edb
akscodebank <akshayappu7147@gmail.com>
2020-08-11 15:26:59 +0530

    hi 3

--- src/Feed.js
+++ src/Feed.js
@@ -1,70 +1,23 @@
 import React from 'react';
+import TweetBox from "./TweetBox";
 import "./Feed.css";
 
 function Feed() {
     return (
         <div className="feed">
             {/* Header */}
-            <div classsName="feed">
+            <div classsName="feed_header">
             <h2>Home</h2>
             </div>
 
             {/* TweetBox */}
+            <TweetBox />
+
             <h3>Post #</h3>
             <h3>Post #</h3>
             <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
-            <h3>Post #</h3>
+       
+    
             {/* Post */}
             {/* Post */}
             {/* Post */}
